746b3e70914ecc2b998387a9b2827bef11749160
anbumk999 <anbumk999@gmail.com>
2026-07-01 15:12:16 +0530

    Navigation completed

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useSelector } from 'react-redux';
 
 import SplashScreen from '../screens/Splash/SplashScreen';
 import AuthNavigator from './AuthNavigator';
@@ -9,14 +8,8 @@ import MainTabNavigator from './MainTabNavigator';
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
-  const rootScreen = useSelector(
-    (state: any) => state.auth.rootScreen,
-  );
-
   return (
-    <Stack.Navigator
-      initialRouteName={rootScreen}
-      screenOptions={{ headerShown: false }}>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Splash" component={SplashScreen} />
       <Stack.Screen name="AuthNavigator" component={AuthNavigator} />
       <Stack.Screen name="MainTabNavigator" component={MainTabNavigator} />
--- src/screens/Splash/SplashScreen.tsx
+++ src/screens/Splash/SplashScreen.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
-import { Text, Button } from 'react-native';
+import React, { useEffect } from 'react';
+import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useSelector } from 'react-redux';
 
 const SplashScreen = ({ navigation }: any) => {
+  const rootScreen = useSelector(
+    (state: any) => state.auth.rootScreen
+  );
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.reset({
+        index: 0,
+        routes: [{ name: rootScreen }],
+      });
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <SafeAreaView style={{ flex: 1 }}> 
-        <Text>Splash Screen</Text>
-        <Button
-          title="Go To Login"
-          onPress={() => navigation.navigate('AuthNavigator')}
-        />
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>Splash Screen</Text>
     </SafeAreaView>
   );
 };
